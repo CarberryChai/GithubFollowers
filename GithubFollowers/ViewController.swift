752f477bbfed63f50aa87c6ae1a4ea665a3052fc
Changlin <CarberryChai@gmail.com>
2024-01-01 12:26:45 +0800

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by changlin on 2024/1/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var count = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
