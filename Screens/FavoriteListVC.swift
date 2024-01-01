977cc0c896b18c54b853df8dfec53aaffe9b094b
Changlin <CarberryChai@gmail.com>
2024-01-01 13:22:21 +0800

    add Tabbar controller

---
+++ Screens/FavoriteListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteListVC.swift
+//  GithubFollowers
+//
+//  Created by changlin on 2024/1/1.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
