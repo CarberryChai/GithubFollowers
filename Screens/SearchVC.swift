7c75d5714a012bd3d8fc5eb49724736ee99b749c
Changlin <CarberryChai@gmail.com>
2024-01-01 20:35:07 +0800

    update search page

--- Screens/SearchVC.swift
+++ Screens/SearchVC.swift
@@ -7,12 +7,47 @@
 
 import UIKit
 
-class SearchVC: UIViewController {
+class SearchVC: UIViewController, UITextFieldDelegate {
+    let logo = UIImageView()
+    let usernameTextField = GFTextField()
+    let actionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        configureLogo()
+        configureTextField()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+}
+
+extension SearchVC {
+    private func configureLogo() {
+        view.addSubview(logo)
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.image = UIImage(named: "gh-logo")
+        NSLayoutConstraint.activate([
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.heightAnchor.constraint(equalToConstant: 200),
+            logo.widthAnchor.constraint(equalToConstant: 200)
+        ])
+
+    }
+
+    private func configureTextField() {
+        view.addSubview(usernameTextField)
+        usernameTextField.delegate = self
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 }
