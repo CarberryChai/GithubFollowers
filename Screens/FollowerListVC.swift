7830ff0b30ee228c13a134b7b2caa8865854577e
Changlin <CarberryChai@gmail.com>
2024-01-10 20:35:31 +0800

    network manager

--- Screens/FollowerListVC.swift
+++ Screens/FollowerListVC.swift
@@ -13,11 +13,18 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+                case .success(let followers):
+                    print("followers count: \(followers.count)")
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Bad Request", message: error.localizedDescription, buttonTitle: "OK")
+            }
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
 }
